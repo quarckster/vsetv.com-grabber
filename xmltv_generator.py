897462327379afd8d7f290cd89c32aba7dd3e931
quarckster <quarckster@gmail.com>
2014-12-06 08:25:28 +0700

    using more lists comprehensive

--- xmltv_generator.py
+++ xmltv_generator.py
@@ -13,18 +13,10 @@ class VsetvGrabber:
         self.main_selector = self.g.doc.select('//div[@id="schedule_container"]')
         
     def get_channels_titles_dict(self):
-        channels_titles = []
-        i = 1
-        for elem in self.g.doc.select('//td[@class="channeltitle"]').text_list():
-            channels_titles.append({'display-name': [(elem, u'ru')], 'id': str(i)})
-            i += 1
-        return channels_titles
+        return [{'display-name': [(elem, u'ru')], 'id': str(i+1)} for i,elem in enumerate(self.g.doc.select('//td[@class="channeltitle"]').text_list())]
      
     def get_starttime(self):
-        starttime = []
-        for i in range(self.amount_channels):
-            starttime.append(self.main_selector[i].select('./div[@class="time" or @class="onair" or @class="pasttime"]').text_list())
-        return starttime
+        return [self.main_selector[i].select('./div[@class="time" or @class="onair" or @class="pasttime"]').text_list() for i in range(self.amount_channels)]
      
     def convert_date(self, start_time):
         dt = str(date.today()) + " " + str(start_time)
@@ -46,20 +38,16 @@ class VsetvGrabber:
      
     def get_stoptime(self):
         new_time = []
+        t = time(5, 0)
+        d = date.today() + timedelta(days=1)
+        dt = datetime.combine(d, t)
         for i in self.correct_starttime():
-            i = i[1:]
-            t = time(5, 0)
-            d = date.today() + timedelta(days=1)
-            dt = datetime.combine(d, t)
-            i.append(dt)
+            i[1:].append(dt)
             new_time.append(i)
         return new_time
      
     def get_programmes_titles(self):
-        title = []
-        for i in range(self.amount_channels):
-            title.append(self.main_selector[i].select('./div[@class="prname2" or @class="pastprname2"]').text_list())
-        return title
+        return [self.main_selector[i].select('./div[@class="prname2" or @class="pastprname2"]').text_list() for i in range(self.amount_channels)]
      
     def time_to_string(self, datetime_object):
         return datetime_object.strftime("%Y%m%d%H%M%S")
@@ -78,9 +66,9 @@ class VsetvGrabber:
         for i in self.get_channels_titles_dict():
             w.addChannel(i)
         for i in self.make_dict():
-             w.addProgramme(i)
+            w.addProgramme(i)
         w.write(file)
-
+        
 if __name__ == '__main__':
     x = VsetvGrabber()
     x.write('tv_guide.xml')
