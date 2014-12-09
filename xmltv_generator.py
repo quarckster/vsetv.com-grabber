d76db2d84568a0abc1250692499743061c0e070e
Dmitry Misharov <quarckster@gmail.com>
2014-12-09 11:26:40 +0700

    small refactoring

--- xmltv_generator.py
+++ xmltv_generator.py
@@ -9,14 +9,14 @@ class VsetvGrabber:
         self.g = Grab()
         self.g.setup(cookies={"cll":"your_cookie", "cp":"your_cookie"})
         self.g.go('http://www.vsetv.com/schedule_package_personal_day_' + str(date.today()) + '_nsc_1.html')    
-        self.amount_channels = len(self.g.doc.select('//div[@id="schedule_container"]'))
+        self.amount_channels = range(len(self.g.doc.select('//div[@id="schedule_container"]')))
         self.main_selector = self.g.doc.select('//div[@id="schedule_container"]')
         
     def get_channels_titles_dict(self):
         return [{'display-name': [(elem, u'ru')], 'id': str(i+1)} for i,elem in enumerate(self.g.doc.select('//td[@class="channeltitle"]').text_list())]
      
     def get_starttime(self):
-        return [self.main_selector[i].select('./div[@class="time" or @class="onair" or @class="pasttime"]').text_list() for i in range(self.amount_channels)]
+        return [self.main_selector[i].select('./div[@class="time" or @class="onair" or @class="pasttime"]').text_list() for i in self.amount_channels]
      
     def convert_date(self, start_time):
         dt = str(date.today()) + " " + str(start_time)
@@ -27,14 +27,16 @@ class VsetvGrabber:
         return [map(self.convert_date, p) for p in self.get_starttime()]
      
     def correct_starttime(self):
-        wrong_datetime = self.make_date()
-        for nested_datetime_stack in wrong_datetime:
-            for idx, this_date in enumerate(nested_datetime_stack):
-                if ((idx + 1) != len(nested_datetime_stack)):
-                    next_date = nested_datetime_stack[(idx + 1) % len(nested_datetime_stack)]
+        starttime_lists = self.make_date()
+        for starttime in starttime_lists:
+            for i, this_date in enumerate(starttime):
+                try:
+                    next_date = starttime[i+1]
                     if this_date > next_date:
-                        nested_datetime_stack[(idx + 1) % len(nested_datetime_stack)] += timedelta(days=1)
-        return wrong_datetime
+                         starttime[i+1] += timedelta(days=1)
+                except IndexError:
+                    continue
+        return starttime_lists
      
     def get_stoptime(self):
         new_time = []
@@ -48,7 +50,7 @@ class VsetvGrabber:
         return new_time
      
     def get_programmes_titles(self):
-        return [self.main_selector[i].select('./div[@class="prname2" or @class="pastprname2"]').text_list() for i in range(self.amount_channels)]
+        return [self.main_selector[i].select('./div[@class="prname2" or @class="pastprname2"]').text_list() for i in self.amount_channels]
      
     def time_to_string(self, datetime_object):
         return datetime_object.strftime("%Y%m%d%H%M%S")
